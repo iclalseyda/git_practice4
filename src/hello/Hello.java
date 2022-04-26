662f802142a985946296a2e2ec65e52f3694a057
seyda <iclalseyda@gmail.com>
2022-04-27 00:39:25 +0200

    doing final work on hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,8 +1,10 @@
 package hello;
 
 public class Hello {
+
     public static void main(String[] args) {
         System.out.println(" hello world");
+        System.out.println("Learning GIT github");
     }
 
 }
